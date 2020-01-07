cbe3ac3cd9ea2331cca64df595bb603f4172ecdd
robbiesanchez <rfsanchez004@gmail.com>
2020-01-07 09:48:42 -0500

    kata9

--- katas.js
+++ katas.js
@@ -88,3 +88,13 @@ function litres(time) {
     return Math.floor(time * 0.5);
   }
 
+// 9
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+// Task :
+// Given a year, return the century it is in.
+
+function century(year) {
+    return Math.ceil(year/100); 
+  }
